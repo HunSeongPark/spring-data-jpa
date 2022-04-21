2684d3a9d3718732e0d0ed5fc0b1cdbbd7411c41
Hunseong <phunseong@gmail.com>
2022-04-22 00:57:25 +0900

    Entity test

---
+++ src/test/java/hello/springjpa/domain/MemberTest.java
@@ -0,0 +1,53 @@
+package hello.springjpa.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Hunseong on 2022/04/22
+ */
+@SpringBootTest
+@Transactional
+class MemberTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    void testEntity() {
+        
+        // given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        em.flush();
+        em.clear();
+
+        // when & then
+        List<Member> result = em.createQuery("select m from Member m", Member.class).getResultList();
+        for (Member member : result) {
+            System.out.println("member = " + member);
+            System.out.println("member.team = " + member.getTeam());
+        }
+    }
+}
