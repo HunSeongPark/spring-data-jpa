82aa64c7d72805daf178c8f1b5c7a11069042569
Hunseong <phunseong@gmail.com>
2022-04-22 01:35:20 +0900

    순수 Jpa Repository Paging

--- src/main/java/hello/springjpa/repository/MemberJpaRepository.java
+++ src/main/java/hello/springjpa/repository/MemberJpaRepository.java
@@ -53,4 +53,19 @@ public class MemberJpaRepository {
                 .setParameter("age", age)
                 .getResultList();
     }
+
+    // ====== Paging ====== //
+    public List<Member> findByPage(int age, int offset, int limit) {
+        return em.createQuery("select m from Member m where m.age = :age order by m.username desc", Member.class)
+                .setParameter("age", age)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    public long totalCount(int age) {
+        return em.createQuery("select count(m) from Member m where m.age = :age", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+    }
 }
