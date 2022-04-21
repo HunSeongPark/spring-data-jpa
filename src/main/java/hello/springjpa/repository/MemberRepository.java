b2e49fa6b7481bdb8b14f1dcba824424deaac87b
Hunseong <phunseong@gmail.com>
2022-04-22 01:14:14 +0900

    순수 JPA JPQL 쿼리 메서드

--- src/main/java/hello/springjpa/repository/MemberRepository.java
+++ src/main/java/hello/springjpa/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package hello.springjpa.repository;
 import hello.springjpa.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Created by Hunseong on 2022/04/22
  */
